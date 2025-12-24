45054d52fdd0b8c9a6743ecd89af0738ef55322b
pavan kalyan <pavan667204@gmail.com>
2025-12-24 18:06:24 +0530

    docker

--- mysql/scripts/schema.sql
+++ mysql/scripts/schema.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS transactions (
     description VARCHAR(255)
 );
 
-CREATE USER IF NOT EXISTS 'expense'@'%' IDENTIFIED BY 'expense@123';
+CREATE USER IF NOT EXISTS 'expense'@'%' IDENTIFIED BY 'ExpenseApp@1';
 GRANT ALL ON transactions.* TO 'expense'@'%';
 FLUSH PRIVILEGES;
